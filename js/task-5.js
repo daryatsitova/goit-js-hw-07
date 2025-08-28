2c28618e34522725ad25f3a1d38f44e989c809cd
citauka <daryaatsitova@icloud.com>
2025-08-28 19:20:39 +0200

    task 4-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
